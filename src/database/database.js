cb1ce1f2ef0e8f79452cb30085fb5dc46ecdb6c8
Alex B <49875526+Truthwatcher64@users.noreply.github.com>
2024-11-30 23:45:36 -0700

    Additions to the pipeline: now has new default admin, also ignores some tests

--- src/database/database.js
+++ src/database/database.js
@@ -335,7 +335,7 @@ class DB {
         }
 
         if (!dbExists) {
-          const defaultAdmin = { name: '常用名字', email: 'a@jwt.com', password: 'admin', roles: [{ role: Role.Admin }] };
+          const defaultAdmin = { name: config.adminUser.username, email: config.adminUser.email, password: config.adminUser.password, roles: [{ role: Role.Admin }] };
           this.addUser(defaultAdmin);
         }
       } finally {
