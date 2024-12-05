fe3c37b76a01997cba6d135a0bbd6cb421b7cf65
Alex B <49875526+Truthwatcher64@users.noreply.github.com>
2024-12-05 05:10:37 -0700

    Repush

--- src/logger.js
+++ src/logger.js
@@ -104,17 +104,3 @@ class Logger {
 module.exports = new Logger();
 
 
-
-// HTTP requests
-// HTTP method, path, status code
-// If the request has an authorization header
-// Request body
-// Response body
-
-// Database requests
-// SQL queries
-
-// Factory service requests
-
-// Any unhandled exceptions
-// Sanitize all log entries so that they do not contain any confidential information
